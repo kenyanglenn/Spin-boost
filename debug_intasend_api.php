1f91c33fcd7f0f9bcf2f593c19eb9b451b959b3e
kenyanglenn <glennmsanii1@gmail.com>
2026-04-17 13:09:39 +0300

    Delete debug_intasend_api.php

--- debug_intasend_api.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * IntaSend API Debug Script - REMOVED
- * All payment integration code has been cleaned up.
- * This file is now a placeholder.
- */
-
-header('Content-Type: application/json');
-echo json_encode(['message' => 'Payment integration code removed', 'status' => 'cleaned']);
